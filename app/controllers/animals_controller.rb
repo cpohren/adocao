cbc5c7649b60a35bd7c3c4e3cc3cd9478701b1c3
Carolina Pohren <carol.pohren@gmail.com>
2018-05-06 01:11:42 -0300

    add create animal method

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -11,6 +11,18 @@ class AnimalsController < ApplicationController
     def new
       @animal = Animal.new
     end
+    def create
+      @animal = Animal.new(animal_params)
+      respond_to do |format|
+        if @animal.save(animal_params)
+          format.html { redirect_to edit_animal_path(@animal), notice: 'Device was successfully updated.' }
+          format.json { render :show, status: :ok, location: @animal }
+        else
+          format.html { render :edit }
+          format.json { render json: @animal.errors, status: :unprocessable_entity }
+        end
+      end
+    end
     def update
       @animal = Animal.find(params[:id])
         respond_to do |format|
