67a4f18f659033e3e5d177cbbd2384a9b023dbc3
Carolina Pohren <carolpohren@sou.faccat.br>
2018-06-16 02:23:48 +0000

    traducao do filtro

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,14 @@ module Adocao
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Brasilia'
+
+    config.i18n.enforce_available_locales = true
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{yml}')]
+    config.i18n.available_locales = ['en', 'pt-BR']
+
+    config.i18n.default_locale = :'en'
   end
 end
